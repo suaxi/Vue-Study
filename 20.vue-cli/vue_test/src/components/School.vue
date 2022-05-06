244dbb4be67f119bb165e4e70147b2ef30b0120b
wanghao <281463547@qq.com>
2022-05-06 21:55:31 +0800

    vue-cli及render函数

---
+++ 20.vue-cli/vue_test/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ schoolName }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SchoolName',
+    data() {
+      return {
+        schoolName: '家里蹲',
+        address: '北京'
+      }
+    }
+  }
+</script>
+
+<style>
+  .demo{
+    background-color: azure;
+  }
+</style>
