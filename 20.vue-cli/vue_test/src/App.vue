bf6915f6fd39416bebc6885d9ea79df0f38b1619
wanghao <281463547@qq.com>
2022-05-08 22:36:49 +0800

    todoList案例初始化、添加操作

--- 20.vue-cli/vue_test/src/App.vue
+++ 20.vue-cli/vue_test/src/App.vue
@@ -1,21 +1,82 @@
 <template>
-  <div>
-    <school/>
-    <hr>
-    <Student/>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo"/>
+        <List :todos="todos"/>
+        <MyFooter/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import School from './components/School'
-import Student from './components/Student'
+import MyHeader from './components/MyHeader'
+import List from './components/List'
+import MyFooter from './components/MyFooter'
 
 export default {
   name: "App",
-  components: {School,Student}
+  components: {MyHeader,List,MyFooter},
+  data() {
+    return {
+      todos: [
+        {id:'001',title:'吃饭',done:true},
+        {id:'002',title:'睡觉',done:false},
+        {id:'003',title:'打豆豆',done:true}
+      ]
+    }
+  },
+  methods: {
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 
 </style>
