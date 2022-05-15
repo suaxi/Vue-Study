e31d12fe6b275bfb54a4edb9c5d8283ea9322bc0
wanghao <281463547@qq.com>
2022-05-15 20:56:50 +0800

    默认插槽

---
+++ 20.vue-cli/vue_test/22.插槽/1.src_默认插槽/components/Category.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <!-- 定义一个插槽，当使用者没有传递具体结构时，使用默认值填充 -->
+    <slot>默认插槽</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyCategory",
+  props: ['title']
+}
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+h3 {
+  background-color: yellowgreen;
+  text-align: center;
+}
+
+img {
+  width: 100%;
+}
+
+video {
+  width: 100%;
+}
+</style>
