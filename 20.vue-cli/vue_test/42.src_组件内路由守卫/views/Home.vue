6bc75a9e48acd3a283c6cf8117bcfec631932f3e
wanghao <281463547@qq.com>
2022-05-21 21:58:47 +0800

    组件内路由守卫

---
+++ 20.vue-cli/vue_test/42.src_组件内路由守卫/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Home组件内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link class="list-group-item" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <!--路由缓存，include写的必须是组件名-->
+      <keep-alive include="MyNews">
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHome"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 20.vue-cli/vue_test/42.src_组件内路由守卫/views/News.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul>
+    <li :style="{opacity}">欢迎学习Vue</li>
+    <li>news001 <input type="text"></li>
+    <li>news002 <input type="text"></li>
+    <li>news003 <input type="text"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "MyNews",
+  data() {
+    return {
+      opacity: 1
+    }
+  },
+  /* mounted() {
+    this.timer = setInterval(() => {
+      this.opacity -= 0.01
+      if (this.opacity <= 0) {
+        this.opacity = 1
+      }
+    })
+  }, */
+  /* beforeDestroy() {
+    console.log('News组件即将被销毁了')
+    clearInterval(this.timer)
+  } */
+
+  //激活
+  activated() {
+    console.log('News组件激活')
+    this.timer = setInterval(() => {
+      console.log('@')
+      this.opacity -= 0.01
+      if (this.opacity <= 0) {
+        this.opacity = 1
+      }
+    })
+  },
+
+  //失活
+  deactivated() {
+    console.log('News组件失活')
+      clearInterval(this.timer)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
